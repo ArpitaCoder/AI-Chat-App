6d541614d7a0fe9864777513e5ded33bd20d88a5
Arpita Sharma <your - arpitasharma93101@gmail.com>
2026-08-27 22:19:51 +0530

    Connect React frontend with Express backend

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,18 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("Hello this is backend");
+})
+
+app.post("/api/chat/",(req,res)=>{
+    console.log(res.body);
+    res.json({
+        message: "Hello from backend"
+    })
+
 })
 
 app.listen(PORT,()=>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,18 @@ import './App.css'
 function App() {
   const [message,setMessage] = useState("Hello Here is Your response");
   const connectBackend=async()=>{
-    const response = await fetch("http://localhost:5000/");
-    const data = await response.text();
+    const response = await fetch("http://localhost:5000/api/chat",{
+      method: "POST",
+      headers: {
+        "content-Type":"application/json",
+      },
+      body:JSON.stringify({
+        message: "Hello backend",
+      }),
+    })
+    const data = await response.json();
 
-    setMessage(data);
+    setMessage(data.message);
   }
    return(
     <>
