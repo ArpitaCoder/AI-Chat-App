775cb660de9d185907f745142d6204d57bb6ef8d
Arpita Sharma <your - arpitasharma93101@gmail.com>
2026-08-28 19:57:17 +0530

    Added Gemini API

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const {GoogleGenAI} = require("@google/genai")
+
 const app = express();
 const PORT = process.env.PORT;
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+})
 
 app.use(cors())
 app.use(express.json())
@@ -12,12 +18,24 @@ app.get("/",(req,res)=>{
     res.send("Hello this is backend");
 })
 
-app.post("/api/chat/",(req,res)=>{
-    console.log(req.body.message);
-    res.json({
-        message: "Hello This is backend"
+app.post("/api/chat/",async(req,res)=>{
+    // console.log(req.body.message);
+    try{
+        const response = await ai.models.generateContent({
+            model: "gemini-3.6-flash",
+            contents: "Explain apple in one sentence."
+        })
+
+        res.json({
+        message: response.text
     })
 
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({
+            message:"Something went wrong"
+        })
+    }
 })
 
 app.listen(PORT,()=>{
