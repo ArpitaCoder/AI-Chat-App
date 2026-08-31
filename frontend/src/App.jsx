0dd96646cc0baa6eb41ed75165db0ead0a9b7aca
Arpita Sharma <your - arpitasharma93101@gmail.com>
2026-08-31 14:25:47 +0530

    Build basic chat interface and backend integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [message,setMessage] = useState("Hello Here is Your response");
+  const [messages,setMessages] = useState([]);
   const [input,setInput] = useState("")
 
   const connectBackend=async()=>{
@@ -18,7 +18,18 @@ function App() {
     })
     const data = await response.json();
 
-    setMessage(data.message);
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "user",
+        text: input,
+      },
+      {
+        role: "ai",
+        text: data.message,
+      }
+    ]);
+    setInput("")
 
   }
    return(
@@ -27,7 +38,13 @@ function App() {
     <input type="text" placeholder='Type your message...' value={input} onChange={(e)=>setInput(e.target.value)} />
 
     <button onClick = {connectBackend}>Send</button>
-    <p>{message}</p>
+    <div>
+      {messages.map((msg,index)=>(
+        <p key={index}>
+          <strong>{msg.role}:</strong>{msg.text}
+        </p>
+      ))}
+    </div>
     </>
     
    )
